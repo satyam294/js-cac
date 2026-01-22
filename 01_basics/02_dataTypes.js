daeedb2b302fc9a1bbc856705d593b01b4f8265e
satyam294 <satyamchand294@gmail.com>
2026-01-22 17:52:53 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict" //treats all js code as newer version
+//alert(2 + 5) //we are using node js , not browser
+
+let name = "Satyam"
+let age = 15
+let isLoggedIn = false
+
+/*
+numbers: 2 ^ 53
+bigInt: not used often
+String: prefer double quotes""
+boolean: true/false
+null: standalone value
+undefined: value not assigned yet
+symbol: unique
+
+objects: 
+*/
+console.log(typeof age)
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
