189333576c32e722ad8e5536fce0c1ec608fa4a6
satyam294 <satyamchand294@gmail.com>
2026-01-24 11:00:14 +0000

    stack and heap memory

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -39,3 +39,32 @@ console.log(typeof(outsideTemp))  //null has the object type
 //all non premitive data types have "object" type
 // functions have "function" type
 // bigNumber has "bigint" type
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//stack and heap
+
+//stack(primitive data types)       |  heap(non primitive data types)
+//a copy of the object is passed    |  the object reference is passed
+// no change in the original object |  any change is reflected in the original object
+
+let name1 = "Satyam"    //primitive, inititated in stack
+let name2 = name1       //name2 is given a copy of name1, not the original object
+name2 = "JinPing"       //name2 changed, no effect on name1, as a copy passed
+console.log(name1)       
+console.log(name2)      //PASS BY VALUE !!!! a copy passed, no change in og
+
+let user1 = {             //object created in heap, reference "user1" in stack
+    name: "Satyam",
+    number: 112233
+}
+
+let user2 = user1          // same object in heap assigned to reference "user2"
+
+user2.number = 123456      //change made to the og object
+
+console.log(user2.number)
+console.log(user1.number)   //PASS BY REFERENCE !!!!! shared object, changes og obj
+
+// PRIMITIVE : STACK, PASS BY VALUE
+// NON PRIMITIVE : HEAP, PASS BY REFERENCE
